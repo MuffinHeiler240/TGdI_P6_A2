1d798db27ce143b7280f886144221caac7eab999
MuffinHeiler240 <jonathangreive@gmail.com>
2025-01-10 09:12:52 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,60 @@
+// *********************************************************************
+// A5_lm75.c									Version 05.01.2025  hpw
+// Frostwarner
+// Dokumentation bcm2835:
+// https://www.airspayce.com/mikem/bcm2835/index.html
+// *********************************************************************
+#include <stdio.h>					// Header fuer Ein-/Ausgaben
+#include "bcm2835.h"					    // Header fuer Broadcom
+
+#define		PIN				RPI_BPLUS_GPIO_J8_12						// LED-Pin
+#define		I2C_ADR			0x48										// Adresse LM75
+
+int main(void){
+    if(bcm2835_init()==0) return 1;										// Library initalisieren
+
+    // Pin als output einstellen
+
+    // I2C initalisieren
+    if (bcm2835_i2c_begin()==0) return 1;
+
+    // I2C Adresse einstellen
+    bcm2835_i2c_setSlaveAddress(I2C_ADR);
+
+    // I2C Mode einstellen
+    bcm2835_i2c_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_4096);
+
+
+    printf("Temperatursensor LM75 wird ausgelesen...\n");
+    // In Schleife
+    while(1) {
+        char buf[2] = {0};
+
+        // Daten lesen und ausgeben
+        bcm2835_i2c_read(buf, 2);
+
+        #pragma GCC diagnostic push
+        #pragma GCC diagnostic ignored "-Wconversion"
+        int16_t temp = (buf[0] << 8) | buf[1];
+        #pragma GCC diagnostic pop
+
+        temp >>= 5;
+        double temperature = temp * 0.125;
+        printf("Aktuelle Temperatur: %.2f°C\n", temperature);
+
+        // LED an- bzw. ausschalten
+        if (temperature < 0){
+            bcm2835_gpio_set(PIN);}
+        else{
+            bcm2835_gpio_clr(PIN);}
+
+        // Pause
+        bcm2835_delay(1000);
+    }
+
+    // I2C-Pins auf Standard
+    bcm2835_i2c_end();
+
+    if(bcm2835_close()==0) return 1;									// Library schließen
+    return 0;							// Programmende
+}
